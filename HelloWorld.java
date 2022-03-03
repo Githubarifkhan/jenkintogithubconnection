d3e6b6aee903a3ae5853fea1441e5de1b3ff2557
Arif Khan <yakhwajagaribnamaz@gmail.com>
2022-03-03 17:47:01 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello jenkins i am coming from github, i am lucky guy"); 
         System.out.println("i am a funniest guy!!!");
+        System.out.println("i am a coolest guy");
         
     }
 }
